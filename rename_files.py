c396185c98979e3b518fb13b747cdff12bf01d13
Jorge Mimica <jmimica@gmail.com>
2016-11-18 18:36:47 -0200

    Update code

--- rename_files.py
+++ rename_files.py
@@ -9,8 +9,15 @@ def rename_files():
 	# (1) get filenames from a given folder
 	file_list = os.listdir(r"/Users/mimica/Google Drive/CodeDevelopment/Udacity/ud036 Fundamentos da Programacao com Python/prank")
 	print(file_list)
-	
+	saved_path = os.getcwd();
+	os.chdir(r"/Users/mimica/Google Drive/CodeDevelopment/Udacity/ud036 Fundamentos da Programacao com Python/prank")
+
 	# (2) for each file, rename it, removing numbers from the name
+	for file_name in file_list:
+		print("Old Name - "+file_name)
+		print("New Name - "+file_name.translate(None, "0123456789"))
+		os.rename(file_name, file_name.translate(None, "0123456789"))
+	os.chdir(saved_path)
 
 print("Running ...")
 rename_files()
