2c64306082e5f217f7cb38a1981988ccf40595a0
Jorge Mimica <jmimica@gmail.com>
2016-11-17 23:46:26 -0200

    Add messages to console

--- break_time.py
+++ break_time.py
@@ -3,13 +3,15 @@ import webbrowser
 
 count = 0
 
+print("This program started on "+time.ctime())
 while (count < 3):
 	# Wait for 2 hours
 	time.sleep(10)
 
 	# Open the browser
 	webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+	print("   opening the video at "+time.ctime())
 
 	count = count + 1
 
-print"Bye bye. See you ..."
+print("Bye bye. See you ...")
