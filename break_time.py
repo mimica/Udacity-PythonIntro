f1005a250cee1a7d6c8890ddeb5286dc68bbc6fb
Jorge Mimica <jmimica@gmail.com>
2016-11-17 23:42:51 -0200

    1st version of program

---
+++ break_time.py
@@ -0,0 +1,15 @@
+import time
+import webbrowser
+
+count = 0
+
+while (count < 3):
+	# Wait for 2 hours
+	time.sleep(10)
+
+	# Open the browser
+	webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+	count = count + 1
+
+print"Bye bye. See you ..."
